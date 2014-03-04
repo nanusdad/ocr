1775ac9e403b7bbadb2d4128194ac372013161a8
EC2 Default User <ec2-user@ip-10-185-80-237.ec2.internal>
2014-03-04 20:29:39 +0000

    Changed image file variable

--- cr.js
+++ cr.js
@@ -1,7 +1,8 @@
 var nodecr = require('nodecr');
+var tmp_jpg = '/tmp/image.jpg';
 
 // Recognise text of any language in any format
-nodecr.process('/path/to/image.jpg',function(err, text) {
+nodecr.process(tmp_jpg,function(err, text) {
     if(err) {
         console.error(err);
     } else {
@@ -12,7 +13,7 @@ nodecr.process('/path/to/image.jpg',function(err, text) {
 // Need to install language packs in
 // /usr/local/share/tessdata
 // Recognise Eng text in a single uniform block of text
-nodecr.process('/path/to/image.jpg',function(err, text) {
+nodecr.process(tmp_jpg,function(err, text) {
     if(err) {
         console.error(err);
     } else {
